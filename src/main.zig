e07d53dec3029f1687995f856f768a66c03298f7
Shantanu Raj <s@sraj.me>
2024-10-27 22:43:54 +0100

    chore(img): Embed stub file

--- src/main.zig
+++ src/main.zig
@@ -4,29 +4,56 @@ const c = @cImport({
     @cInclude("stb_image.h");
 });
 
+const stubImage = @embedFile("fixtures/norway_hut.jpg");
+
+const File = struct {
+    allocator:  ?std.mem.Allocator,
+    contents: []const u8,
+
+    pub fn deinit(self: *const File) void {
+        if (self.allocator == null) return;
+        const allocator = self.allocator.?;
+        allocator.free(self.contents);
+    }
+};
+
+fn get_file_contents(allocator: std.mem.Allocator, args: []const []const u8) !File {
+    if (args.len < 2) {
+        return File{
+            .allocator = null,
+            .contents = std.mem.sliceAsBytes(stubImage),
+        };
+    }
+
+    const file = try std.fs.cwd().openFile(args[1], .{ .mode = .read_only });
+    defer file.close();
+
+    const file_size = try file.getEndPos();
+
+    const file_contents = try file.readToEndAlloc(allocator, file_size);
+    return File{
+        .allocator = allocator,
+        .contents = file_contents,
+    };
+}
+
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
     const args = try std.process.argsAlloc(allocator);
     defer allocator.free(args);
 
-    const file =
-        if (args.len > 1) try std.fs.openFileAbsolute(args[1], .{ .mode = .read_only })
-        else try std.fs.cwd().openFile("./src/fixtures/norway_hut.jpg", .{});
-    defer file.close();
+    const file = try get_file_contents(allocator, args);
+    defer file.deinit();
 
-    const file_size = try file.getEndPos();
-
-    const file_contents = try file.readToEndAlloc(allocator, file_size);
-    defer allocator.free(file_contents);
-
+    const file_contents = file.contents;
     std.debug.print("Bytes read: {}\n", .{file_contents.len});
 
     var width: c_int = 0;
     var height: c_int = 0;
     var channels: c_int = 0;
 
-    const file_size_c: c_int = @intCast(file_size);
+    const file_size_c: c_int = @intCast(file_contents.len);
 
     const img = c.stbi_load_from_memory(file_contents.ptr, file_size_c, &width, &height, &channels, 0);
     defer c.stbi_image_free(img);
